901e82997106f81b1f237e9c72fa007ff1c93a4a
neelmyna <nithincheeta@gmail.com>
2025-02-18 11:02:19 +0530

    day1 programs added

---
+++ day1/even_number.py
@@ -0,0 +1,8 @@
+# Check if a number is Even or not.
+
+input_number = int(input('Enter a number to check if it is Even: '))
+
+if input_number % 2 == 0:
+    print(f'{input_number} is Even number')
+else:
+    print(f'{input_number} is not an Even number')
